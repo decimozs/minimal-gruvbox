98aa32e56659d8961c2642d6c8bf94da0629bac4
decimozs <marlonadiguemartin548@gmail.com>
2024-04-22 19:37:44 +0800

    neovim config

--- index.js
+++
@@ -1,9 +0,0 @@
-const array = [1, 2, 3, 4, 5];
-
-for (let index = 0; index < array.length; index++) {
-  if (array[index] % 2 == 0) {
-    console.log("Even Numbers");
-  } else {
-    console.log("Odd Numbers");
-  }
-}
